a2bce5e4b7a2c7ed68a89af71b1b399aac8e3771
kelvin <kelvin_ma7@163.com>
2021-07-05 08:56:35 +0800

    feat createDemo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { Tabs, List } from 'antd';
+import React from 'react';
 
-function App() {
+const { TabPane } = Tabs;
+
+function callback(key) {
+  console.log(key);
+}
+export default class Todo extends React.Component {
+  state = {
+    todo: [1,2,3],
+    complete: [4,5,6]
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <MyTabs todo={this.state.todo} complete={this.state.complete}></MyTabs>
+        </header>
+      </div>
+    )
+  }
+}
+function ItemList (props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <List
+      dataSource={props.data}
+      renderItem={item => (
+          <List.Item>
+            { item }
+          </List.Item>
+        )
+      }
+    >
+    </List>
+  )
 }
 
-export default App;
+function MyTabs (props) {
+  console.log(props);
+  return (
+    <Tabs defaultActiveKey="1" onChange={callback}>
+      <TabPane tab="待办" key="todo">
+        <ItemList data={props.todo}></ItemList>
+      </TabPane>
+      <TabPane tab="已完成" key="complete">
+        <ItemList data={props.complete}></ItemList>
+      </TabPane>
+    </Tabs>
+  )
+}
